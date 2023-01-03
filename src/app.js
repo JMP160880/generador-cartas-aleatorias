63a16d0cc22a7481e1de239983d509cd08423dac
JMP160880 <javiermoranpando@gmail.com>
2023-01-03 19:09:10 +0000

    clase 3-1-23

--- src/app.js
+++ src/app.js
@@ -1,11 +1,40 @@
 /* eslint-disable */
-import "bootstrap";
 import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  dibujarCarta();
+};
+let generateRandomNumber = () => {
+  let numbers = [
+    "A",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let indexNumbers = Math.floor(Math.random() * numbers.length);
+  return numbers[indexNumbers];
+};
+let generateRandomPalo = () => {
+  let palo = ["diamond", "spade", "heart", "club"];
+  let indexPalo = Math.floor(Math.random() * palo.length);
+  return palo[indexPalo];
+};
+let dibujarCarta = () => {
+  generateRandomNumber();
+  let paloGenerado = generateRandomPalo();
+  let palo1 = document.querySelector(".top-palo");
+  let palo2 = document.querySelector(".bottom-palo");
+  palo1.innerHTML = paloGenerado;
+  palo2.innerHTML = paloGenerado;
 };
